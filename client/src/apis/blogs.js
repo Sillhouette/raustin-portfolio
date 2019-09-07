40546bcbba59cb5feefc49fc1d9ef5f91acff34f
github username <raustin.melchior@gmail.com>
2019-09-07 12:47:20 -0700

    fix the request url for blog posts attempt #2

--- client/src/apis/blogs.js
+++ client/src/apis/blogs.js
@@ -5,5 +5,5 @@ import axios from "axios";
  * Create an instance of axios to interact with our rails backend
  **/
 export default axios.create({
-  baseURL: "http://localhost:3001/api/v1"
+  baseURL: "http://www.austinmelchior.com/api/v1"
 });
