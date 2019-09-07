d2980976966c25db7b0b8c849a974e182414103a
github username <raustin.melchior@gmail.com>
2019-09-06 23:08:07 -0700

    fix routing when not going to root attempt #4

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :blog_posts
-  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
-    !request.xhr? && request.format.html?
-  end
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+  });
+  # get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+  #   !request.xhr? && request.format.html?
+  # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
