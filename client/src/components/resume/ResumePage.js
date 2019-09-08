b58d8e2179d12275f33a8f659417daa018ed7d1e
github username <raustin.melchior@gmail.com>
2019-09-08 09:03:55 -0700

    responsive pdf for resume attempt #4

--- client/src/components/resume/ResumePage.js
+++ client/src/components/resume/ResumePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container } from "semantic-ui-react";
 import Filler from "../common/Filler";
-import { Document } from "react-pdf/dist/entry.webpack";
+import { Document } from "react-pdf";
 import resume from "./pdfs/Austin-Melchior-Resume.pdf";
 
 const ResumePage = () => (
@@ -10,7 +10,7 @@ const ResumePage = () => (
     <div style={{ width: 600 }}>
       <Document
         file={resume}
-        onLoadSuccess={this.onDocumentLoadSuccess}
+        onLoadSuccess={this.onDocumentLoadSuccess && console.log("PDF LOADED")}
       ></Document>
     </div>
     <Filler num="3" />
