31bcbc85cae3f9d333c4fa0085d615df55e89c3a
github username <raustin.melchior@gmail.com>
2019-09-07 21:36:35 -0700

    remove header menu from home page attempt #9

--- client/src/components/common/Header.js
+++ client/src/components/common/Header.js
@@ -4,57 +4,60 @@ import { Link } from "react-router-dom";
 import { Menu } from "semantic-ui-react";
 import "./Header.css";
 
-const Header = props => (
-  <Menu
-    className={props.visible ? null : "invisible"}
-    id="header"
-    compact
-    stackable
-    pointing
-    secondary
-    inverted
-  >
-    <Menu.Item
-      as={Link}
-      to="/"
-      fitted
-      name="home"
-      active={props.activeItem === "home"}
-      onClick={props.handleItemClick}
-    />
-    <Menu.Item
-      as={Link}
-      to="/projects"
-      fitted
-      name="projects"
-      active={props.activeItem === "projects"}
-      onClick={props.handleItemClick}
-    />
-    <Menu.Item
-      as={Link}
-      to="/resume"
-      fitted
-      name="resume"
-      active={props.activeItem === "resume"}
-      onClick={props.handleItemClick}
-    />
-    <Menu.Item
-      as={Link}
-      to="/about"
-      fitted
-      name="about"
-      active={props.activeItem === "about"}
-      onClick={props.handleItemClick}
-    />
-    <Menu.Item
-      as={Link}
-      to="/blog"
-      fitted
-      name="blog"
-      active={props.activeItem === "blog"}
-      onClick={props.handleItemClick}
-    />
-  </Menu>
-);
+const Header = props => {
+  debugger;
+  return (
+    <Menu
+      className={props.visible ? null : "invisible"}
+      id="header"
+      compact
+      stackable
+      pointing
+      secondary
+      inverted
+    >
+      <Menu.Item
+        as={Link}
+        to="/"
+        fitted
+        name="home"
+        active={props.activeItem === "home"}
+        onClick={props.handleItemClick}
+      />
+      <Menu.Item
+        as={Link}
+        to="/projects"
+        fitted
+        name="projects"
+        active={props.activeItem === "projects"}
+        onClick={props.handleItemClick}
+      />
+      <Menu.Item
+        as={Link}
+        to="/resume"
+        fitted
+        name="resume"
+        active={props.activeItem === "resume"}
+        onClick={props.handleItemClick}
+      />
+      <Menu.Item
+        as={Link}
+        to="/about"
+        fitted
+        name="about"
+        active={props.activeItem === "about"}
+        onClick={props.handleItemClick}
+      />
+      <Menu.Item
+        as={Link}
+        to="/blog"
+        fitted
+        name="blog"
+        active={props.activeItem === "blog"}
+        onClick={props.handleItemClick}
+      />
+    </Menu>
+  );
+};
 
 export default Header;
