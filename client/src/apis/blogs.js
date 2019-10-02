02bb9576dde86e472390de0169a13d49716c48d6
github username <raustin.melchior@gmail.com>
2019-10-01 17:49:03 -0700

    add port number for axios

--- client/src/apis/blogs.js
+++ client/src/apis/blogs.js
@@ -5,5 +5,5 @@ import axios from "axios";
  * Create an instance of axios to interact with our rails backend
  **/
 export default axios.create({
-  baseURL: "http://134.209.52.55/api/v1"
+  baseURL: "http://134.209.52.55:3000/api/v1"
 });
