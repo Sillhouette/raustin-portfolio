5d40d60568e607e46097620da0ac3c42ccfed955
github username <raustin.melchior@gmail.com>
2019-10-01 17:19:14 -0700

    add cors config

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,12 @@ module PortfolioWebsite
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.middleware.insert_before 0, Rack::Cors do
+          allow do
+             origins 'http://www.stream-source.net'
+             resource '*', :headers => :any, :methods => :any
+           end
+        end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
