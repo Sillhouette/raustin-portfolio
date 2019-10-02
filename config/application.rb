9549f095f016ba3b0cd2ee2865b76608b999f151
github username <raustin.melchior@gmail.com>
2019-10-01 17:35:14 -0700

    add cors exception

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module PortfolioWebsite
     config.load_defaults 5.2
     config.middleware.insert_before 0, Rack::Cors do
           allow do
-             origins 'http://www.stream-source.net'
+             origins 'http://134.209.52.55'
              resource '*', :headers => :any, :methods => :any
            end
         end
