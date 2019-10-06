73a2618141ac1e628386827a6b5113917a0f458d
github username <raustin.melchior@gmail.com>
2019-10-05 23:17:33 -0700

    fix typo in image display

--- client/src/components/common/ImageDisplay.js
+++ client/src/components/common/ImageDisplay.js
@@ -7,8 +7,8 @@ export default class ImageDisplay extends Component {
 
   handleItemClick = (e, { name }) => {
     const image = document.getElementById("project-image");
-    const source1 = document.getElementsByClassName("source")[0];
-    const source2 = document.getElementsByClassName("source")[1];
+    const source1 = document.getElementsByClassName("project-image-source")[0];
+    const source2 = document.getElementsByClassName("project-image-source")[1];
     if (name === "first") {
       image.setAttribute("src", this.props.firstImage);
       source1.setAttribute("srcset", this.props.firstImageWeb);
