0769e2fbc98a4c9b6a9b847709cd1cb0b81ad98b
github username <raustin.melchior@gmail.com>
2019-10-05 23:08:58 -0700

    get elements by tag name instead of class name for image slider

--- client/src/components/common/ImageDisplay.js
+++ client/src/components/common/ImageDisplay.js
@@ -7,8 +7,8 @@ export default class ImageDisplay extends Component {
 
   handleItemClick = (e, { name }) => {
     const image = document.getElementById("project-image");
-    const source1 = document.getElementsByClassName()[0];
-    const source2 = document.getElementsByClassName()[1];
+    const source1 = document.getElementsByTagName("source")[0];
+    const source2 = document.getElementsByTagName("source")[1];
     if (name === "first") {
       image.setAttribute("src", this.props.firstImage);
       source1.setAttribute("srcset", this.props.firstImageWeb);
