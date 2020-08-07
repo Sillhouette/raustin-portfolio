08f78de65457b8459035782b7eae4c9346faddbb
Austin Melchior <raustin.melchior@gmail.com>
2020-08-07 10:31:56 -0700

    look at site conf

--- config/initializers/unsplash_api.rb
+++ config/initializers/unsplash_api.rb
@@ -3,7 +3,6 @@ Unsplash.configure do |config|
   config.application_secret = ENV['UNSPLASH_ACCESS_SECRET']
   #config.application_redirect_uri = "https://your-application.com/oauth/callback"
   config.utm_source = "portfolio-site"
-  puts ENV["SECRET_KEY_BASE"]
   # optional:
   # config.logger = MyCustomLogger.new
 end
